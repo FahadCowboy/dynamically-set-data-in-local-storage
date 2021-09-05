be7fad905d186e5d2f1015f785063469129605e8
FahadCowboy <olivierafahad@gmail.com>
2021-09-06 02:16:06 +0600

    Almost done the project

--- localStorage.js
+++ localStorage.js
@@ -6,14 +6,19 @@ const displayFromLocal = () => {
 
 }
 
+// This function is for adding 'add items' botton
 const addItems = () => {
     const inputField = document.getElementById('input')
     const name = inputField.value
+    if(!name){
+        return
+    }
     inputField.value = ''
     displayItems(name)
     setLocalData(name)
 }
 
+//this function is to display items in the UI
 const displayItems = name => {
     const ul = document.getElementById('cart-items')
     const li = document.createElement('li')
@@ -21,6 +26,7 @@ const displayItems = name => {
     ul.appendChild(li)
 }
 
+//This function bring us the cart object if local storage has it, otherwise, it give us an empty object for further work
 const getLocalData = () => {
     const cartObj = localStorage.getItem('cart')
     let parsedCartObj
@@ -33,11 +39,24 @@ const getLocalData = () => {
     return parsedCartObj
 }
 
+//This function used to set data in the cart object associet in the local storage 
 const setLocalData = name => {
     const localData = getLocalData()
-    localData[name] = 1
+
+    if(localData[name]){
+        localData[name] = localData[name] + 1
+    } else{
+        localData[name] = 1 
+    }
+    
     const stringifiedData = JSON.stringify(localData)
     localStorage.setItem('cart', stringifiedData)
+}
+
+//This function is used to clear all the data from UI and local storage
+const placeOrder = () => {
+    document.getElementById('cart-items').textContent = ''
+    localStorage.removeItem('cart')
 }
 
 
