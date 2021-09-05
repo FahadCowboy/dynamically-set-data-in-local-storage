72908a9451afdced3e211b577a42e25232f7a293
FahadCowboy <olivierafahad@gmail.com>
2021-09-06 00:03:45 +0600

    showing data in ui and storring data in local storage is completed, a little validation is remained

--- localStorage.js
+++ localStorage.js
@@ -1,9 +1,17 @@
+const displayFromLocal = () => {
+    const localData = getLocalData()
+    for(const data in localData){
+        displayItems(data)
+    }
+
+}
+
 const addItems = () => {
     const inputField = document.getElementById('input')
     const name = inputField.value
     inputField.value = ''
-
     displayItems(name)
+    setLocalData(name)
 }
 
 const displayItems = name => {
@@ -12,3 +20,25 @@ const displayItems = name => {
     li.innerText = name
     ul.appendChild(li)
 }
+
+const getLocalData = () => {
+    const cartObj = localStorage.getItem('cart')
+    let parsedCartObj
+    if(cartObj) {
+        parsedCartObj = JSON.parse(cartObj)
+    } else{
+        parsedCartObj = {}
+    }
+
+    return parsedCartObj
+}
+
+const setLocalData = name => {
+    const localData = getLocalData()
+    localData[name] = 1
+    const stringifiedData = JSON.stringify(localData)
+    localStorage.setItem('cart', stringifiedData)
+}
+
+
+displayFromLocal()
